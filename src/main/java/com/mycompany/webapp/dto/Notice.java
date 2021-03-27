06dfa36026556ab5455ba31328e6eb25a068f30a
jjung-222 <lyj7877@naver.com>
2021-03-27 20:47:36 +0900

    dao

--- src/main/java/com/mycompany/webapp/dto/Notice.java
+++ src/main/java/com/mycompany/webapp/dto/Notice.java
@@ -1,5 +1,50 @@
 package com.mycompany.webapp.dto;
 
-public class Notice {
+import java.util.Date;
 
+public class Notice {
+	private int boardno;
+	private String uid;
+	private String btitle;
+	private Date bdate;
+	private String bcontent;
+	private int bcount;
+	
+	
+	public int getBoardno() {
+		return boardno;
+	}
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public Date getBdate() {
+		return bdate;
+	}
+	public void setBdate(Date bdate) {
+		this.bdate = bdate;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public int getBcount() {
+		return bcount;
+	}
+	public void setBcount(int bcount) {
+		this.bcount = bcount;
+	}
 }
--- src/main/java/com/mycompany/webapp/service/CommunityQnasService.java
+++ src/main/java/com/mycompany/webapp/service/CommunityQnasService.java
@@ -1,8 +1,58 @@
 package com.mycompany.webapp.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.CommunityQnasDao;
+import com.mycompany.webapp.dto.CommunityQna;
+import com.mycompany.webapp.dto.Pager;
 
 @Service
 public class CommunityQnasService {
+	private static final Logger logger = 
+			LoggerFactory.getLogger(CommunityQnasService.class);
+	@Autowired
+	private CommunityQnasDao communityqnasDao;
+	
+	public List<CommunityQna> getBoardList() {
+		List<CommunityQna> list = communityqnasDao.selectAll();
+		return list;
+	}
+	
+	public List<CommunityQna> getBoardList(Pager pager) {
+		List<CommunityQna> list = communityqnasDao.selectByPage(pager);
+		return list;
+	}	
+	
+	public void saveBoard(CommunityQna communityqna) {
+		logger.info("저장전 bno:"+ communityqna.getBoardno());
+		communityqnasDao.insert(communityqna);
+		logger.info("저장 후 bno:" + communityqna.getBoardno());
+	}
 
+	public CommunityQna getBoard(int boardno) {
+		CommunityQna communityqna = communityqnasDao.selectByBoardno(boardno);
+		return communityqna;
+	}
+
+	public void updateBoard(CommunityQna communityqna) {
+		communityqnasDao.update(communityqna);
+	}
+	
+	public void deleteBoard(int boardno) {
+		communityqnasDao.deleteByBoardno(boardno);
+	}
+	
+	public void addBcount(int boardno) {
+		communityqnasDao.updateBcount(boardno);
+	}
+
+	public int getTotalRows() {
+		int rows = communityqnasDao.count();
+		return rows;
+	}
 }
