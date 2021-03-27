b1af8dc738ef58d6af5d21c6f95f3d99cdb7ee07
sora0420 <thfk0420@naver.com>
2021-03-27 19:52:27 +0900

    product

--- src/main/java/com/mycompany/webapp/service/ProductsService.java
+++ src/main/java/com/mycompany/webapp/service/ProductsService.java
@@ -1,8 +1,30 @@
 package com.mycompany.webapp.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dto.Products;
 
 @Service
 public class ProductsService {
-
+	@Autowired ProductsService productsDao;
+	public List<Products> pSelectAll(){
+		List<Products> list = productsDao.pSelectAll();
+		return list;
+	}	
+	public void pInsert(Products products) {
+		productsDao.pInsert(products);
+	};
+	public void pUpdate(Products products) {
+		productsDao.pUpdate(products);
+	};
+	public Products pSelectByPno(int productno) {
+		Products products = productsDao.pSelectByPno(productno);
+		return products;
+	};
+	public void pDeleteByPno(int productno) {
+		productsDao.pDeleteByPno(productno);
+	}
 }
