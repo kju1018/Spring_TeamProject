87a299b9ed92a7038fc47f19309f5889a684049d
sora0420 <thfk0420@naver.com>
2021-03-27 20:23:52 +0900

    productqna

--- src/main/java/com/mycompany/webapp/dao/ProductQnasDao.java
+++ src/main/java/com/mycompany/webapp/dao/ProductQnasDao.java
@@ -1,5 +1,16 @@
 package com.mycompany.webapp.dao;
 
-public class ProductQnasDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mycompany.webapp.dto.ProductQnas;
+
+@Mapper
+public interface ProductQnasDao {
+	public List<ProductQnas> pQanSelectAll(); //게시판 목록 전체
+	public ProductQnas pQnaInsert(ProductQnas productqnas); //게시판 입력
+	public int pQnaUpdate(ProductQnas productqnas); //게시판 업데이트
+	public int pQnaDelete(int boardno); //게시판 삭제
+	
 }
