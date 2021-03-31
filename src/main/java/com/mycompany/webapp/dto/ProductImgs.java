68fc38c8b18f402f61b1708d23ec50802f5512c9
sora0420 <thfk0420@naver.com>
2021-03-31 15:28:26 +0900

    메인 경로 수정 / product / productlist

--- src/main/java/com/mycompany/webapp/dto/ProductImgs.java
+++ src/main/java/com/mycompany/webapp/dto/ProductImgs.java
@@ -6,4 +6,42 @@ public class ProductImgs {
 	private String ioriginalname;
 	private String isavename;
 	private String imgtype;
+	private int ipriority;
+
+	public int getImgno() {
+		return imgno;
+	}
+	public void setImgno(int imgno) {
+		this.imgno = imgno;
+	}
+	public int getProductno() {
+		return productno;
+	}
+	public void setProductno(int productno) {
+		this.productno = productno;
+	}
+	public String getIoriginalname() {
+		return ioriginalname;
+	}
+	public void setIoriginalname(String ioriginalname) {
+		this.ioriginalname = ioriginalname;
+	}
+	public String getIsavename() {
+		return isavename;
+	}
+	public void setIsavename(String isavename) {
+		this.isavename = isavename;
+	}
+	public String getImgtype() {
+		return imgtype;
+	}
+	public void setImgtype(String imgtype) {
+		this.imgtype = imgtype;
+	}
+	public int getIpriority() {
+		return ipriority;
+	}
+	public void setIpriority(int ipriority) {
+		this.ipriority = ipriority;
+	}
 }
