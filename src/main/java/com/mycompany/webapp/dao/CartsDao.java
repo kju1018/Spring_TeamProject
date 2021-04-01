7b9d22cd6dc784f28e2f76c021226ea5b2c14cdc
kju1018 <kms951018@naver.com>
2021-04-01 17:10:03 +0900

    수정

--- src/main/java/com/mycompany/webapp/dao/CartsDao.java
+++ src/main/java/com/mycompany/webapp/dao/CartsDao.java
@@ -11,8 +11,9 @@ public interface CartsDao {
 	public int insert(Cart cart);
 	public Cart select(Cart cart);
 	public List<Cart> selectByUserId(String userid);
-	public int delete(List<Cart> cartlist);//선택삭제
+	public int deleteSelectCart(List<Cart> cartlist);//선택삭제
 	public int deleteAllByUserId(String userid);//전체 삭제
+	public int updateCartQuantity(Cart cart);
 	public int deleteByCart(Cart cart); //한개 삭제
-	public int updateCart(Cart cart);
+
 }
