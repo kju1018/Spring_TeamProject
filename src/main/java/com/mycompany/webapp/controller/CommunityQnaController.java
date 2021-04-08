6d542c9cea25681772f1e76d919b09f4d7bf7967
sihyunpark <bags26578@gmail.com>
2021-04-08 14:48:06 +0900

    시큐리티적용, login failure handler 적용, 이미지 경로 적용

--- src/main/java/com/mycompany/webapp/controller/CommunityQnaController.java
+++ src/main/java/com/mycompany/webapp/controller/CommunityQnaController.java
@@ -109,7 +109,7 @@ public class CommunityQnaController {
 		return "community/my_qna_list";
 	}
 	
-	@GetMapping("/qna_write")
+	@GetMapping("/qna_write") // 막기
 	public String communityQnaWrite(Model model, Authentication auth) {
 		String userid = "";
 		userid = auth.getName();
@@ -120,7 +120,7 @@ public class CommunityQnaController {
 		return "community/qna_write";
 	}
 
-	@PostMapping("/create")
+	@PostMapping("/create")  //막기
 	public String communityQnaCreate(CommunityQna communityqna, HttpSession session, Model model, Authentication auth) {
 		communityQnasService.saveBoard(communityqna);
 		
@@ -152,7 +152,7 @@ public class CommunityQnaController {
 		return "community/myqna_view";
 	}
 
-	@GetMapping("/qna_update")
+	@GetMapping("/qna_update") //막기
 	public String communityQnaUpdateForm(int boardno, Model model) {
 		CommunityQna communityqna = communityQnasService.getBoard(boardno);
 		model.addAttribute("communityqna", communityqna);
@@ -160,21 +160,21 @@ public class CommunityQnaController {
 		return "community/qna_update";
 	}
 
-	@PostMapping("/updateqna")
+	@PostMapping("/updateqna")//막기
 	public String communityQnaUpdate(CommunityQna communityqna) {
 		communityQnasService.updateBoard(communityqna);
 		
 		return "redirect:/community/qna_view?boardno=" + communityqna.getBoardno();
 	}
 
-	@GetMapping("/deleteqna")
+	@GetMapping("/deleteqna") //막기
 	public String communityQnaDelete(int boardno) {
 		communityQnasService.deleteBoard(boardno);
 		
 		return "redirect:/community/qna_list";
 	}
 
-	@GetMapping("/answer_write")
+	@GetMapping("/answer_write") // 막기
 	public String AnswerqnaWrite(int boardno, Authentication auth, Model model) {
 		String userid = "";
 		userid = auth.getName();
@@ -185,7 +185,7 @@ public class CommunityQnaController {
 		return "community/answer_write";
 	}
 
-	@PostMapping("/createrepl")
+	@PostMapping("/createrepl") // 막기
 	public String communityReplCreate(CommunityQna communityqna, HttpSession session, Model model, Authentication auth) {
 		communityqna.setBcount(0);
 		communityqna.setGroupord(0);
@@ -197,7 +197,7 @@ public class CommunityQnaController {
 
 	}
 	
-	@GetMapping("/answer_view")
+	@GetMapping("/answer_view") // 막기
 	public String answerView(int boardno, Model model, Authentication auth) {
 		communityQnasService.addBcount(boardno);
 		CommunityQna communityqna = communityQnasService.getBoard(boardno);
--- src/main/webapp/WEB-INF/views/auth/login.jsp
+++ src/main/webapp/WEB-INF/views/auth/login.jsp
@@ -5,17 +5,12 @@
 
 	<h1 style="color:red;">${requestScope.loginFailMsg}</h1>
    <div class="container-lg item_mt-18">
-		   <c:if test="${not empty SPRING_SECURITY_LAST_EXCEPTION}">
+		  
+			    <c:if test="${empty errormsg}">
 	               <div class="text_center_sort">
-					   	 <font color="red">
-					        <p>로그인에 실패하였습니다.<br/>
-					           이유 : ${sessionScope["SPRING_SECURITY_LAST_EXCEPTION"].message}</p>
-					        <c:remove var="SPRING_SECURITY_LAST_EXCEPTION" scope="session"/>
-					     </font>
+					   
 				    </div>
-			   </c:if>
-		
-		
+			   </c:if> 
    	  <form method="post" action="<%=application.getContextPath()%>/login">
    	  	<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
 
@@ -28,13 +23,27 @@
                     <input type="text" class="form-control" id="userid" name="userid" placeholder="ID">
                 </div>
                 <!--비밀번호 입력-->
-                <div class="item_mt-1" >
+                <div class="item_mt-05" >
                     <input type="password" class="form-control" id="upassword" name="upassword" placeholder="PASSWORD">
                 </div>
+                <c:if test="${not empty errormsg}">
+	               <div class="item_mt-05">
+					   	 <font color="red">
+					           원인 :  ${errormsg}</p>
+					        <c:remove var="SPRING_SECURITY_LAST_EXCEPTION" scope="session"/>
+					     </font>
+				    </div>
+			   </c:if> 
+			   <c:if test="${empty errormsg}">
+	               <div class="item_mt-05">
+					   	
+				    </div>
+			   </c:if> 
                 <!--로그인 버튼-->
-                <div class="item_mt-1">
+                <div >
                     <button type="submit" class="btn btn-dark btn-lg item_width_100 btn-block">로그인</button>
                 </div>
+               
                 <!-- span 태그-->
                 <div class="item_mt-05  item_pl-1">
                 	<a href="<%=application.getContextPath()%>/auth/find_id"><span class="fontsize-sm">아이디 찾기 |</span></a>
@@ -44,7 +53,7 @@
                 <div class="item_mt-05 item_mb-3">
                     <a href="<%=application.getContextPath()%>/auth/signup" class="btn btn-secondary btn-lg item_width_100 btn-block">회원가입</a>
                 </div>
-            
+              
                 
             </div>
         </div>
